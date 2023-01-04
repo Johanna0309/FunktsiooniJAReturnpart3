32a6f715395695d8db3fddfff56dd76ed966955c
Johanna Avik <68692132+Johanna0309@users.noreply.github.com>
2023-01-04 20:02:30 +0200

    Create For-loop.cs

---
+++ For-loop.cs
@@ -0,0 +1,14 @@
+//FOR-LOOP
+//DRY-principle - Do not repeat yourself 
+
+int i = 0;
+
+for(int i = 0; i < 5; i++) //i++ = i + 1
+{
+    Console.WriteLine("We don't need no education.");
+}
+
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day.");
+
+//Console.Write Line("We don't need no education.");
